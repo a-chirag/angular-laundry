96f87a033f2df49167ee82c45055a9f6dff7ad22
chirag <a.chirag@outlook.com>
2018-02-24 12:54:00 +0530

    Initial

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,66 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
-
+import { HttpClientModule} from '@angular/common/http';
+import { FormsModule } from '@angular/forms';
 import { AppComponent } from './app.component';
+import { OrdersComponent } from './orders/orders.component';
+import { OrderDetailsComponent } from './order-details/order-details.component';
+import { AppRoutingModule } from './/app-routing.module';
+import { ClientService } from './client.service';
+import { OrdersService } from './orders.service';
+import { MatMenuModule, MatNativeDateModule } from '@angular/material';
+import { MatButtonModule } from '@angular/material/button';
+import {MatTableModule} from '@angular/material/table';
+import {MatCardModule} from '@angular/material/card';
+import {MatPaginatorModule} from '@angular/material/paginator';
+import {MatSortModule} from '@angular/material/sort';
+import {MatToolbarModule} from '@angular/material/toolbar';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import {MatFormFieldModule} from '@angular/material/form-field';
+import {MatInputModule} from '@angular/material/input';
+import {MatChipsModule} from '@angular/material/chips';
+import {MatDatepickerModule} from '@angular/material/datepicker';
+import { NgControl, FormControl, ReactiveFormsModule } from '@angular/forms';
+import { ClientsComponent } from './clients/clients.component';
+import { AddOrderComponent } from './add-order/add-order.component';
+import {MatAutocompleteModule} from '@angular/material/autocomplete';
+import {MatSelectModule} from '@angular/material/select';
 
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    OrdersComponent,
+    OrderDetailsComponent,
+    ClientsComponent,
+    AddOrderComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FormsModule,
+    AppRoutingModule,
+    HttpClientModule,
+    MatCardModule,
+    MatTableModule,
+    MatPaginatorModule,
+    MatSortModule,
+    MatToolbarModule,
+    MatMenuModule,
+    MatButtonModule,
+    BrowserAnimationsModule,
+    MatFormFieldModule,
+    MatInputModule,
+    MatChipsModule,
+    MatDatepickerModule,
+    MatNativeDateModule,
+    MatAutocompleteModule,
+    ReactiveFormsModule,
+    MatSelectModule
   ],
-  providers: [],
+  exports: [
+    MatAutocompleteModule
+  ],
+  providers: [OrdersService, ClientService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
